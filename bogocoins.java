36ce5e7c35d47d8acdc7647cebf11caa11f00a8c
GorlakTD <31046033+GorlakTD@users.noreply.github.com>
2017-12-05 10:26:23 -0500

    Update bogocoins.java

--- bogocoins.java
+++ bogocoins.java
@@ -6,20 +6,30 @@ public class Bogocoins
   private ArrayList<Coin> source = new ArrayList<Coin>();
   private int target = 30;
   private int numPennies;
-	private int numNickels;
-	private int numDimes;
-	private int numQuarters;
+  private int numNickels;
+  private int numDimes;
+  private int numQuarters;
   public void transmute()
   {
+	
     for(int i = 0; i < c.size; i++)
     {
       for(int k = 0; k < c.get(i).size; k++)
       {
-        if (c.get(i).get(k).getType() == 'p');
-				{
-					c.get(i).remove(k);
-					numPennies ++
-				}
+        if (c.get(i).get(k).getType() == 'p')
+		{
+			cVal = c.get(i).get(k).
+			c.get(i).remove(k);
+		}
+      }
+	  for(int k = 0; k < c.get(i).size; k++)
+	  {
+		if (cVal == 5)
+			c.get(i).add('n');
+		else if (cVal == 10)
+			c.get(i).add('d');
+		else if (cVal == 25)
+			c.get(i).add('q');
       }
     }
   }
@@ -28,6 +38,5 @@ public class Bogocoins
     for (int i = 0; i < target; i++)
       ref.add(new Coin("Penny"));
     c.add(ref);
-    
   }
 }
