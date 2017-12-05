f6df3477b1bc7ac998b76472a19f6d61265c0f30
GorlakTD <31046033+GorlakTD@users.noreply.github.com>
2017-12-05 12:41:05 -0500

    Update bogocoins.java

--- bogocoins.java
+++ bogocoins.java
@@ -2,36 +2,29 @@
 public class Bogocoins
 {
   private ArrayList<ArrayList<Coin>> c = new ArrayList<ArrayList<Coin>>();
-  private ArrayList<Coin> ref = new ArrayList<Coin>();
+  
   private ArrayList<Coin> source = new ArrayList<Coin>();
   private int target = 30;
   public void transmute()
   {
-	
+ArrayList<Coin> ref = c.get(c.size);
     for(int i = 0; i < c.size; i++)
     {
       for(int k = 0; k < c.get(i).size; k++)
       {
-        if (c.get(i).get(k).getType() == 'p')
-		{
-			cVal = c.get(i).get(k).
-			c.get(i).remove(k);
-		}
-      }
-	  while(cVal > 5)
-	  {
-		if (cVal == 25)
-			c.get(i).add('n');
-		else if (cVal == 10)
-			c.get(i).add('d');
-		else if (cVal == 5)
-			c.get(i).add('q');
-	  }
+	if(ref.get(k).getType() == 'p')
+		ref.set(k, 'n');
+	if(ref.get(k).getType() == 'n')
+		ref.set(k, 'd');
+	if(ref.get(k).getType() == 'd')
+		ref.set(k, 'q');
+	c.add(ref);
       }
     }
   }
   public static void main(String[] args)
   {
+ ArrayList<Coin> ref = new ArrayList<Coin>();	
     for (int i = 0; i < target; i++)
       ref.add(new Coin("Penny"));
     c.add(ref);
